dd31291c87bcabd94bc301a848731878cc10f18e
broman77 <71395385+broman77@users.noreply.github.com>
2026-09-16 00:16:59 +0300

    Bump Molotkova schedule to 0.2.0 beta

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -12,8 +12,8 @@ android {
         applicationId = "ru.mgsu.molotkova.schedule"
         minSdk = 26
         targetSdk = 35
-        versionCode = 1
-        versionName = "0.1.0-beta"
+        versionCode = 2
+        versionName = "0.2.0-beta"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
